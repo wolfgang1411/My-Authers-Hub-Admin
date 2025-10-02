f50a282955c7bad45cf6e966675c09cafa532bab
MOHD SHOAIB <shoaib@uweens.com>
2025-10-02 21:59:10 +0530

    fix(royalty): cleanup and ux fix

--- src/app/components/royalties/royalties.ts
+++ src/app/components/royalties/royalties.ts
@@ -16,7 +16,12 @@ import {
 } from '@angular/forms';
 import { SharedModule } from '../../modules/shared/shared-module';
 import { MatIconModule } from '@angular/material/icon';
-import { Author, Publishers } from '../../interfaces';
+import {
+  Author,
+  ChannalType,
+  PricingGroup,
+  Publishers,
+} from '../../interfaces';
 import { MatTableModule } from '@angular/material/table';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
@@ -25,7 +30,7 @@ import {
   Royalty,
   RoyaltyFormGroup,
 } from '../../interfaces/Royalty';
-import { debounceTime, firstValueFrom } from 'rxjs';
+import { combineLatest, debounceTime, firstValueFrom } from 'rxjs';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -47,6 +52,7 @@ export class Royalties implements OnInit {
 
   royaltiesController =
     input.required<FormArray<FormGroup<RoyaltyFormGroup>>>();
+  pricingController = input.required<FormArray<PricingGroup>>();
   authors = input.required<Author[]>();
   publisher = input.required<Publishers | null>();
 
@@ -83,9 +89,22 @@ export class Royalties implements OnInit {
     ebook_third_party: 0,
   });
 
+  totalRoyaltiesAmount = signal<{
+    authors: Partial<
+      Record<string, Partial<Record<RoyalFormGroupAmountField, number>>>
+    >;
+    publisher: Partial<
+      Record<string, Partial<Record<RoyalFormGroupAmountField, number>>>
+    >;
+  }>({
+    authors: {},
+    publisher: {},
+  });
+
   ngOnInit() {
     this.displayedColumns.set([...this.channelKeys()]);
     this.calculateTotalRoyalties();
+    this.calculateRoyaltyAmountPerPerson();
   }
 
   async validateTotalRoyaltyByType(
@@ -128,14 +147,71 @@ export class Royalties implements OnInit {
       });
   }
 
-  initEmptyRoyalties() {
-    return {
-      print_mah: 0,
-      print_third_party: 0,
-      prime: 0,
-      ebook_mah: 0,
-      ebook_third_party: 0,
-    };
+  calculateRoyaltyAmountPerPerson() {
+    combineLatest([
+      this.royaltiesController().valueChanges,
+      this.pricingController().valueChanges,
+    ])
+      .pipe(debounceTime(400))
+      .subscribe(([data]) => {
+        const temp = this.totalRoyaltiesAmount();
+
+        data.forEach((royalty) => {
+          temp[royalty.authorId ? 'authors' : 'publisher'] = {
+            ...(temp[royalty.authorId ? 'authors' : 'publisher'][
+              royalty.authorId || royalty.publisherId || 0
+            ] = {}),
+          };
+
+          const prices: Partial<Record<RoyalFormGroupAmountField, number>> = {};
+          this.channelKeys().map((key) => {
+            console.log({ key });
+
+            prices[key as RoyalFormGroupAmountField] =
+              this.caculateAmountForRoyaltiesField(
+                key as RoyalFormGroupAmountField,
+                royalty[key as RoyalFormGroupAmountField] || 0
+              );
+          });
+
+          temp[royalty.authorId ? 'authors' : 'publisher'][
+            royalty.authorId || royalty.publisherId || 0
+          ] = prices;
+        });
+
+        this.totalRoyaltiesAmount.set(temp);
+      });
+  }
+
+  caculateAmountForRoyaltiesField(
+    field: RoyalFormGroupAmountField,
+    percent: number
+  ) {
+    let ch: ChannalType;
+    switch (field) {
+      case 'ebook_mah':
+        ch = ChannalType.EBOOK_MAH;
+        break;
+      case 'ebook_third_party':
+        ch = ChannalType.EBOOK_THIRD_PARTY;
+        break;
+      case 'prime':
+        ch = ChannalType.PRIME;
+        break;
+      case 'print_mah':
+        ch = ChannalType.PRINT_MAH;
+        break;
+      case 'print_third_party':
+        ch = ChannalType.EBOOK_THIRD_PARTY;
+        break;
+    }
+
+    const salesPrice =
+      this.pricingController().controls.filter(
+        ({ controls: { channal } }) => channal.value === ch
+      )[0]?.controls?.salesPrice?.value || 0;
+
+    return Number((salesPrice * (percent / 100)).toFixed(2));
   }
 
   submitRoyalties() {
