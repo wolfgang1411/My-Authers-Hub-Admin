1438ef24e64d84bf1feafa052b17713c28989312
Shoaib <shoaibsaifi982@gmail.com>
2025-11-09 12:09:30 +0530

    (fix:title detail)

--- src/app/pages/title-details/title-details.ts
+++ src/app/pages/title-details/title-details.ts
@@ -1,12 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { Back } from '../../components/back/back';
+import { TitleService } from '../titles/title-service';
+import { Title, TitleFilter } from '../../interfaces';
+import { UpperCasePipe } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-title-details',
-  imports: [MatProgressBarModule, MatIconModule, Back],
+  imports: [
+    MatProgressBarModule,
+    MatIconModule,
+    Back,
+    UpperCasePipe,
+    MatButtonModule,
+  ],
   templateUrl: './title-details.html',
   styleUrl: './title-details.css',
 })
-export class TitleDetails {}
+export class TitleDetails {
+  constructor(private titleService: TitleService) {}
+  titles = signal<Title[]>([]);
+  filter: TitleFilter = {
+    page: 1,
+    itemsPerPage: 30,
+    status: 'ALL' as any,
+  };
+  ngOnInit(): void {
+    this.fetchTitleList();
+  }
+
+  fetchTitleList() {
+    this.titleService
+      .getTitles()
+      .then(({ items }) => {
+        const sorted = items.sort((a, b) => {
+          const order = ['DRAFT', 'PENDING', 'APPROVED'];
+          return order.indexOf(a.status) - order.indexOf(b.status);
+        });
+        this.titles.set(sorted);
+        console.log('Fetched & sorted titles:', this.titles());
+      })
+      .catch((error) => {
+        console.error('Error fetching titles:', error);
+      });
+  }
+  draftTitles = computed(() =>
+    this.titles().filter((t) => t.status === 'DRAFT' || t.status === 'PENDING')
+  );
+  publishedTitles = computed(() =>
+    this.titles().filter((t) => t.status !== 'DRAFT' && t.status !== 'PENDING')
+  );
+  deleteTitle(titleId: number) {}
+}
