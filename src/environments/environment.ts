66f6f894c0a21bf39285ee52f0f5168b0e02aef2
MOHD SHOAIB <shoaib@uweens.com>
2025-12-16 00:01:06 +0530

    fix(googleauth): in login

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/',
-  i18nHash: 1762694871194,
+  apiUrl: '',
+  O2AuthClientId: '',
+  i18nHash: 1765823264792,
 };
